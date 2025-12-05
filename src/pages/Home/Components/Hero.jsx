4e71f21774ac9c51e8c63e7b521cf7d2b927c7f5
Xian Saiful <xiansaiful@gmail.com>
2025-12-05 23:35:18 +0600

    Fixed text color

--- src/pages/Home/Components/Hero.jsx
+++ src/pages/Home/Components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className="hero-img max-w-[300px] mx-auto mt-10">
         <img src={ProfileImage} alt="Xian's Creations"></img>
       </div>
-      <div className="text-center max-w-[600px] mx-auto mt-10 space-y-4 px-4">
+      <div className="text-white text-center max-w-[600px] mx-auto mt-10 space-y-4 px-4">
         <h1 className="text-3xl font-bold">Capturing moments that feel real</h1>
         <p>I create soft and natural visuals inspired by everyday moments. Through simple frames and gentle storytelling, I aim to capture feelings that stay with you. This space is where I share the little moments that become memories.</p>
       </div>
